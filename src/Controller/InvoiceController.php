dba8d58a6fab57a2b2259dd42a9bf4c05a1ff73e
Luki1235512 <Luki1235512@gmail.com>
2022-05-02 12:16:00 +0200

    setted up tailwindcss

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -2,20 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Invoice;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InvoiceController extends AbstractController
 {
+
+    private $em;
+
+    public function __construct(EntityManagerInterface $em) {
+        $this->em = $em;
+    }
+
     #[Route('/invoices', name: 'invoices')]
     public function index(): Response
     {
 
-        $invoices = ["inv1", "inv2", "inv3", "inv3"];
+        // $repository = $this->em->getRepository(Invoice::class);
+        // $invoices = $repository->findAll();
 
-        return $this->render('index.html.twig', array(
-            'invoices' => $invoices
-        ));
+        // dd($invoices);
+
+        return $this->render('index.html.twig');
     }
+
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./assets/**/*.{vue,js,jsx,tsx}",
+    "./templates/**/*.{html,twig}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
